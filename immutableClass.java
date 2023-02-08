eaea4765ca7999dd94933092810c11ad5557b131
unknown <webdsnr_sarav@hotmail.com>
2023-02-09 00:12:00 +0530

    Primary commit

---
+++ immutableClass.java
@@ -0,0 +1,10 @@
+// import java.util.*;
+
+final class immutableClass {
+ public static void main(String[] args) {
+   final String newtag = "A";
+   String secTag = "NT";
+   String result = newtag + secTag;
+   System.out.println(result);
+ }
+}
